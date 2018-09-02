49cc5ea941c9ddde215b1c356b52ffab389bbf46
Victor Serrano <victor.serrano@unl.edu.ec>
2018-09-02 07:23:33 -0500

    Update passport.js

--- Proyecto/config/pasaporte/passport.js
+++ Proyecto/config/pasaporte/passport.js
@@ -49,6 +49,13 @@ module.exports = function (passport, cuenta, persona, rol) {
                         Rol.findOne({
                             where: {nombre: 'usuario'}
                         }).then(function (rol) {
+                            
+                            Rol.findOne({where: {nombre:'usuario'}}).then(function(rol){
+                                if(rol){
+                                    res.redirect('/');
+                                }
+                            });
+                            
                             if (rol) {
                                 var dataPersona =
                                         {
