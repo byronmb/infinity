2a5f2a008a3850a80e579a459d96e1e23805e411
Victor Serrano <victor.serrano@unl.edu.ec>
2018-09-02 07:34:01 -0500

    Update passport.js

--- Proyecto/config/pasaporte/passport.js
+++ Proyecto/config/pasaporte/passport.js
@@ -92,76 +92,7 @@ module.exports = function (passport, cuenta, persona, rol) {
             }
     ));
     
-        passport.use('registrarse', new LocalStrategy(
-            {
-                usernameField: 'email', //lo que esta como name en el input del registro
-                passwordField: 'clave', //lo que esta como name en el input del registro
-                passReqToCallback: true // allows us to pass back the entire request to the callback
-            },
-            function (req, email, password, done) {
-                var generateHash = function (password) {
-                    return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-                };
-                //verificar si el email no esta registrado
-                Cuenta.findOne({
-                    where: {
-                        usuario: email
-                    }
-                }).then(function (cuenta) {
-                    if (cuenta)
-                    {
-                        return done(null, false, {
-                            message: 'El correo ya esta registrado'
-                        });
-
-                    } else
-                    {
-                        var userPassword = generateHash(password);
-                        Rol.findOne({
-                            where: {nombre: 'usuario'}
-                        }).then(function (rol) {
-                            if (rol) {
-                                var dataPersona =
-                                        {
-                                            apellidos: req.body.apellidos,
-                                            nombres: req.body.nombres,
-                                            correo: email,
-                                            external_id: uuidv4()
-                                        };
-                                Persona.create(dataPersona).then(function (newPersona, created) {
-                                    if (!newPersona) {
-                                        return done(null, false);
-                                    }
-                                    if (newPersona) {
-                                        var data =
-                                                {
-                                                    usuario: email,
-                                                    clave: userPassword,
-                                                    id_rol: rol.id,
-                                                    id_persona: newPersona.id
-                                                };
-
-                                        Cuenta.create(data).then(function (newCuenta, created) {
-                                            if (!newCuenta) {
-                                                return done(null, false);
-                                            }
-                                            if (newCuenta) {
-                                                return done(null, newCuenta);
-                                            }
-                                        });
-                                    }
-                                });
-                            } else {
-                                return done(null, false, {
-                                    message: 'El rol no existe'
-                                });
-                            }
-                        });
-
-                    }
-                });
-            }
-    ));
+        
     //inicio de sesion
     passport.use('local-signin', new LocalStrategy(
             {
